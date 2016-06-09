e6cdbb22f39d680e6a489aa289a8b4caf9a73fef
jassdv <jasmine.zangi@gmail.com>
2016-06-09 00:17:15 -0400

    update files

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       // 
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl,"randomCategoryShortName",chosenCategoryShortName.short_name);
-      console.log(homeHtmlToInsertIntoMainPage);
+      console.log(homeHtmlUrl);
       
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
