a6d124b528ec1950f89f5cb61bcb6afefcab16bf
jassdv <jasmine.zangi@gmail.com>
2016-06-09 22:37:53 -0400

    update

--- js/script.js
+++ js/script.js
@@ -42,6 +42,7 @@ var insertProperty = function (string, propName, propValue) {
   var propToReplace = "{{" + propName + "}}";
   string = string
     .replace(new RegExp(propToReplace, "g"), propValue);
+    console.log(string);
   return string;
 }
 
@@ -208,7 +209,7 @@ function buildCategoriesViewHtml(categories,
       insertProperty(html, 
                      "short_name",
                      short_name);
-      console.log(html);
+      
     finalHtml += html;
   }
 
